294b013f902e3ee7dab74de090dfd56d81684304
liangysml-dotcom <liangysml@gmail.com>
2026-09-09 07:07:06 +0000

    lab/lab02/ch02_t14_string_formatting_with_percentage_1.py

--- lab/lab02/ch02_t14_string_formatting_with_percentage_1.py
+++ lab/lab02/ch02_t14_string_formatting_with_percentage_1.py
@@ -2,9 +2,3 @@ string_1 = "Camelot"
 string_2 = "place"
 
 print("Let's not go to %s. 'Tis a silly %s." % (string_1, string_2))
-name = "Mike"
-print("Hello %s" % (name))
-
-day = 6
-print('03-%s-2019' % (day))
-print('03-%02d-2019' % (day))
