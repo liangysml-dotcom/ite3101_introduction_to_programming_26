8817111f6658015082abfdd6ca071b995485a483
liangysml-dotcom <liangysml@gmail.com>
2026-09-09 07:10:01 +0000

    [skip actions] lab/lab02/ch02_t15_string_formatting_with_percentage_2.py

--- lab/lab02/ch02_t15_string_formatting_with_percentage_2.py
+++ lab/lab02/ch02_t15_string_formatting_with_percentage_2.py
@@ -4,5 +4,5 @@ color = input("What is your favorite color? ")
 
 # Uncomment the below 2 line of code!
 print("Ah, so your name is ___, your quest is ___, "
-# "and your favorite color is ___." ___ (name, quest, color))
-print()
+"and your favorite color is ___." ___ (name, quest, color))
+
