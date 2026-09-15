df4e6cc835baec5dcf7eccc545ef8d0d62f9ed36
liangysml-dotcom <liangysml@gmail.com>
2026-09-15 03:22:40 +0000

    [skip actions] lab/lab04/ch04_t10_mix_n_match.py

--- lab/lab04/ch04_t10_mix_n_match.py
+++ lab/lab04/ch04_t10_mix_n_match.py
@@ -1,7 +1,7 @@
 # Use boolean expressions as appropriate on the lines below!
 
 # Make me false!
-bool_one = (2 <= 2) and "Alpha" == "Bravo"  # We did this one for you!
+bool_one = 
 
 # Make me true!
 bool_two = None
