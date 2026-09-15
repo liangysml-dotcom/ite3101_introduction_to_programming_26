aa818c3499f632e1398d112d31ddf14d2ef2ba2b
liangysml-dotcom <liangysml@gmail.com>
2026-09-15 02:27:02 +0000

    lab/lab03/ch03_t06_grand_finale.py

--- lab/lab03/ch03_t06_grand_finale.py
+++ lab/lab03/ch03_t06_grand_finale.py
@@ -1,5 +1,5 @@
 from datetime import datetime
 
 now = datetime.now()
-print('%02d/%02d/%04d %02d:%02d:%02d' % (now.month, now.day, now.year, now.hour, now.minute, now.second))
-'%02d:%02d:%02d' % (now.hour, now.minute, now.second))
+print('%02d/%02d/%04d %02d:%02d:%02d' %
+      (now.month, now.day, now.year, now.hour, now.minute, now.second))
