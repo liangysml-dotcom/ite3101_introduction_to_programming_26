075eb460620e3437d8100032fe0ee4665ec3afb5
liangysml-dotcom <liangysml@gmail.com>
2026-09-22 03:12:33 +0000

    lab/lab05/ch05_t08_move_it_back.py

--- lab/lab05/ch05_t08_move_it_back.py
+++ lab/lab05/ch05_t08_move_it_back.py
@@ -4,7 +4,6 @@ pyg = 'ay'
 if len(original) > 0 and original.isalpha():
     word = original.lower()
     first = word[0]
-    new_word = word[1:] + first + pyg
-    print(new_word)
+    new_word = word + first + pyg
 else:
     print("empty")
