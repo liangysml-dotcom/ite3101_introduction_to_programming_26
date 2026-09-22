1b25a9d364a1dbafba232d08e8fbb779b36c7498
liangysml-dotcom <liangysml@gmail.com>
2026-09-22 02:05:07 +0000

    lab/lab04/ch04_t10_mix_n_match.py

--- lab/lab04/ch04_t10_mix_n_match.py
+++ lab/lab04/ch04_t10_mix_n_match.py
@@ -4,12 +4,12 @@
 bool_one = not (5>1)and (3==3)or (10<2)
 
 # Make me true!
-bool_two = not (10>20)and(5==5)or()
+bool_two = not (10>20)and(5==5)or(3<8)
 # Make me false!
 bool_three = not (9<10)and (100<1)and (4+1==5)
 
 # Make me true!
-bool_four = None
+bool_four = not (9<8)
 
 # Make me true!
 bool_five = True
