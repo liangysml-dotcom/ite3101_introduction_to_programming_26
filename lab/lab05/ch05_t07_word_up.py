37e05f4df52b49f35fa81a32a4d09c765560a3bc
liangysml-dotcom <liangysml@gmail.com>
2026-09-22 02:54:57 +0000

    [skip actions] lab/lab05/ch05_t07_word_up.py

--- lab/lab05/ch05_t07_word_up.py
+++ lab/lab05/ch05_t07_word_up.py
@@ -3,7 +3,7 @@ pyg = 'ay'
 original = input('Enter a word:')
 
 if len(original) > 0 and original.isalpha():
-    word = 
+    word = original.lower()
     print(original)
 else:
     print('empty')
