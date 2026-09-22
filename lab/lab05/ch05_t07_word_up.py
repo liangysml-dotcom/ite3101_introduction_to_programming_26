c4dcb18ea50abfa4fcd38212c4c15352df5e23cf
liangysml-dotcom <liangysml@gmail.com>
2026-09-22 02:55:25 +0000

    lab/lab05/ch05_t07_word_up.py

--- lab/lab05/ch05_t07_word_up.py
+++ lab/lab05/ch05_t07_word_up.py
@@ -4,7 +4,7 @@ original = input('Enter a word:')
 
 if len(original) > 0 and original.isalpha():
     word = original.lower()
-    first = word
+    first = word[0]
     print(original)
 else:
     print('empty')
