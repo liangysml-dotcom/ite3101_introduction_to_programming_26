8cfa496ddc844701af46b17ca100b6d521ae34fc
liangysml-dotcom <liangysml@gmail.com>
2026-09-22 02:07:33 +0000

    lab/lab04/ch04_t11_con_statement_syntax.py

--- lab/lab04/ch04_t10_mix_n_match.py
+++ lab/lab04/ch04_t10_mix_n_match.py
@@ -9,7 +9,7 @@ bool_two = not (10>20)and(5==5)or(3<8)
 bool_three = not (9<10)and (100<1)and (4+1==5)
 
 # Make me true!
-bool_four = not (9<8)
+bool_four = not (9<8)and(9==9)and(6+2==8)
 
 # Make me true!
 bool_five = True
